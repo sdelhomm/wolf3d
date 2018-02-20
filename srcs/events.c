86b4eb5c6735c7af9d7260ab1a3d17f51728c8d0
Thomas GUNZBURGER <tgunzbur@e2r7p7.42.fr>
2018-02-20 16:52:30 +0100

    up

--- srcs/events.c
+++ srcs/events.c
@@ -6,14 +6,14 @@
 /*   By: sdelhomm <sdelhomm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 15:37:52 by sdelhomm          #+#    #+#             */
-/*   Updated: 2018/02/20 16:13:37 by tgunzbur         ###   ########.fr       */
+/*   Updated: 2018/02/20 16:52:17 by tgunzbur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "../includes/wolf3d.h"
 
-static void	check_collision(int keycode, t_param *p, t_map map)
+static void	check_collision(int kc, t_param *p, t_map map)
 {
 	double	x;
 	double	y;
@@ -22,13 +22,13 @@ static void	check_collision(int keycode, t_param *p, t_map map)
 	x = p->j.x;
 	y = p->j.y;
 	a = p->j.a * M_PI / 180;
-	a += (keycode == 123 ? -M_PI_2 : 0);
-	a += (keycode == 124 ? M_PI_2 : 0);
-	a += (keycode == 125 ? M_PI : 0);
+	a += (kc == 123 ? -M_PI_2 : 0);
+	a += (kc == 124 ? M_PI_2 : 0);
+	a += (kc == 125 ? M_PI : 0);
 	x = x + HB * cos(a);
 	y = y + HB * sin(a);
-	if (x >= 0 && y >= 0 && x < map.x && y < map.y &&
-		map.map[(int)floor(y + 0.1 * sin(a))][(int)floor(x + 0.1 * cos(a))] != 1
+	if (kc >= 123 && kc <= 126 && x >= 0 && y >= 0 && x < map.x && y < map.y &&
+		map.map[(int)floor(y - 0.2 * sin(a))][(int)floor(x - 0.2 * cos(a))] != 1
 		&& map.map[(int)floor(y)][(int)floor(x)] != 1)
 	{
 		p->j.x = p->j.x + 0.1 * cos(a);
@@ -48,7 +48,7 @@ int			mouse_hook(int x, int y, t_param *p)
 		p->j.a -= 2.5;
 	if (x > p->lx + 1)
 		p->j.a += 2.5;
-	if (x  <= 5 || x >= SCREEN_X - 5)
+	if (x <= 5 || x >= SCREEN_X - 5)
 	{
 		CGWarpMouseCursorPosition(CGPointMake(SCREEN_X / 2 + p->pos_x, SCREEN_Y / 2 + p->pos_y));
 		p->lx = SCREEN_X / 2 - p->pos_x;
@@ -67,8 +67,8 @@ int			key_hook(int keycode, t_param *p)
 		exit(0);
 	if (keycode == 49)
 	{
-		x = (int)floor(p->j.x + cos(p->j.a * M_PI / 180) / 2);
-		y = (int)floor(p->j.y + sin(p->j.a * M_PI / 180) / 2);
+		x = (int)floor(p->j.x + 0.5 * cos(p->j.a * M_PI / 180));
+		y = (int)floor(p->j.y + 0.5 * sin(p->j.a * M_PI / 180));
 		if (x >= 0 && y >= 0 &&
 			x < p->map.x && y < p->map.y && p->map.map[y][x] == 1)
 			p->map.map[(int)floor(y)][(int)floor(x)] = 0;
